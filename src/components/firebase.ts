b0b31e1f932209105bfb77886ec783d3fa2bd8d0
shokkun0407 <shoki1310@gmail.com>
2024-08-10 10:51:31 +0900

    change firebase.ts

--- src/components/firebase.ts
+++ src/components/firebase.ts
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCHrNKX5i3x9IgF8I7VBGSo9yDJRq86MEs",
-  authDomain: "discord-clone-udemy-cd626.firebaseapp.com",
-  projectId: "discord-clone-udemy-cd626",
-  storageBucket: "discord-clone-udemy-cd626.appspot.com",
-  messagingSenderId: "518734870576",
-  appId: "1:518734870576:web:39369d14c699d22affd5f5"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const app = initializeApp(firebaseConfig);
